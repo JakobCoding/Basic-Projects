b847a4308716ca04b0fc771c8c9dc922ea891c21
Jake <jakechalmers86@gmail.com>
2025-05-18 13:55:47 +0800

    Add New Project Passenger Counter App & Corrosponding JS exercises

---
+++ Projects/PassengerCounterApp/exercise.js
@@ -0,0 +1,30 @@
+// Lets learn JavaScript
+
+//Created two variables and print to console
+let message = "Jakes age is"
+let jakesAge = 38
+console.log(message, jakesAge)
+
+/*
+1.Create two variables: myAge and humanDogRatio
+2.Multiply the two together  
+3.store the result in myDogAge 
+4.log myDogAge to console
+*/
+
+let myAge = 38
+let humanDogRatio = 7
+let myDogAge = myAge * humanDogRatio
+console.log(myDogAge)
+
+// Create a Race track Countown Function\
+function countdown() {
+    console.log(5)
+    console.log(4)
+    console.log(3)
+    console.log(2)
+    console.log(1)
+}
+// call the function
+countdown()
+
---
+++ Projects/PassengerCounterApp/index.js
@@ -0,0 +1,10 @@
+
+function increment() {
+    console.log("The button was clicked")
+}
+
+
+
+
+// let count = 0
+// console.log(count)
